cf2a2e44767c94f7a12c21fb6137a1136dc8c2b4
David Pyo <davidpyo@cedarville.edu>
2025-07-25 20:54:24 -0400

    fix drv driver

--- trifolium/src/drvDriver.cpp
+++ trifolium/src/drvDriver.cpp
@@ -9,6 +9,7 @@ Drv::Drv(uint8_t in1, uint8_t in2, uint8_t nsleep_pin, uint8_t mosi_pin, uint8_t
     miso = miso_pin;
     nscs = nscs_pin;
     sclk = sclk_pin;
+    init();
 }
 
 void Drv::init() {
@@ -26,13 +27,18 @@ void Drv::init() {
     pinMode(sclk, OUTPUT);
     pinMode(nscs, OUTPUT);
     digitalWrite(nscs, HIGH);
+    SPI1 = SPIClassRP2040(spi0, miso, nscs, sclk, mosi);
+    /*SPI.setSCK(sclk); //22
+    SPI.setMOSI(mosi); //19
+    SPI.setMISO(miso); //20
+    SPI.setCS(nscs); //21
+    */
+    SPI1.begin();
 
-    SPI.begin();
-
-    if (!wake())
+    while (!wake())
     {
         Serial.println("DRV initialization failed!");
-        return;
+        delay(1000);
     }
 }
 
@@ -41,15 +47,21 @@ bool Drv::wake() {
     delayMicroseconds(2000);
 
     uint16_t devID = readWord(0x00);
+    Serial.print("DRV Device ID: 0x");
+    Serial.println(devID, HEX); 
     if ((devID & 0xc0) == 0) {
         digitalWrite(nsleep, LOW);
+         Serial.println("1!");
         return false;
     }
 
     writeWord(0x08, (1 << 7));
     devID = readWord(0x00);
+    Serial.print("DRV Device ID: 0x");
+    Serial.println(devID, HEX); 
     if ((devID & (1 << 13))) {
         digitalWrite(nsleep, LOW);
+        Serial.println("2!");
         return false;
     }
 
@@ -91,9 +103,9 @@ void Drv::coast() {
 int Drv::writeWord(uint8_t addr, uint8_t data) {
     uint16_t buf = ((addr & 0x3f) << 8) | data;
     digitalWrite(nscs, LOW);
-    SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE1));
-    SPI.transfer16(buf);
-    SPI.endTransaction();
+    SPI1.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE1));
+    SPI1.transfer16(buf);
+    SPI1.endTransaction();
     digitalWrite(nscs, HIGH);
     delayMicroseconds(10);
     return 1;
@@ -102,9 +114,9 @@ int Drv::writeWord(uint8_t addr, uint8_t data) {
 uint16_t Drv::readWord(uint8_t address) {
     uint16_t bufTX = (uint16_t)(((address & 0x3f) | 0x40) << 8);
     digitalWrite(nscs, LOW);
-    SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE1));
-    uint16_t bufRX = SPI.transfer16(bufTX);
-    SPI.endTransaction();
+    SPI1.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE1));
+    uint16_t bufRX = SPI1.transfer16(bufTX);
+    SPI1.endTransaction();
     digitalWrite(nscs, HIGH);
     delayMicroseconds(10);
     return bufRX;
